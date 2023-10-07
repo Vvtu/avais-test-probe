d011032632bb09df64557c1f7dbdc1216985c7e9
vvtuev <vvtuev@gmail.com>
2023-10-07 10:30:28 +0300

    fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,16 +10,12 @@ function App() {
       <div></div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "../reset.d.ts";
-import "./index.css";
-import React from "react";
+import React from 'react';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import ReactDOM from 'react-dom/client';
+
+import App from './App.tsx';
+
+import '../reset.d.ts';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );

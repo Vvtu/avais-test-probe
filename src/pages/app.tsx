970e424003dd25e874960c65adb481fed62565ad
vvtuev <vvtuev@gmail.com>
2023-10-08 18:30:43 +0300

    fix logo animation

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -4,6 +4,8 @@ import { useGetTickets } from '@/hooks/query-hooks';
 import Logo from '@/pages/icons/logo.svg';
 import { ITickets } from '@/services/ticket-validation-scheme';
 
+import cssStyles from './app.css?inline';
+
 export function App() {
   const [count, setCount] = useState(0);
   const result = useGetTickets();
@@ -37,28 +39,38 @@ export function App() {
     });
   }, [allPages]);
 
+  const isLoading = result.isFetching || result.isFetchingNextPage;
+
   console.log('[33m allPages = ', allPages); //TODO - delete vvtu
   console.log('[33m pagesSortedAndFiltered = ', pagesSortedAndFiltered); //TODO - delete vvtu
 
   return (
-    <div style={{ backgroundColor: '#F3F7FA', minHeight: 'calc(100vh - 100px)' }}>
-      <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-        <img src={Logo} alt="Logo icon" height={'60px'} />
-      </div>
-      <div style={{ height: 16, margin: '8px' }}>
-        {(result.isFetching || result.isFetchingNextPage) && <div>Loading...</div>}
-      </div>
-
-      <h1 style={{ marginTop: 40 }}>Vite + React + Typescript +++</h1>
-      <div className="card">
-        <button
-          onClick={async () => {
-            setCount((c) => c + 1);
-          }}
+    <>
+      <style>{cssStyles}</style>
+      <div style={{ backgroundColor: '#F3F7FA', minHeight: 'calc(100vh - 100px)' }}>
+        <div style={{ height: 0 }}>&nbsp;</div>
+        <div
+          style={{ width: '100%', marginTop: '50px', display: 'flex', justifyContent: 'center' }}
         >
-          count is {count}
-        </button>
+          <img
+            src={Logo}
+            alt="Logo icon"
+            height={'60px'}
+            className={isLoading ? 'animate-loading' : ''}
+          />
+        </div>
+
+        <h1 style={{ marginTop: 40 }}>Vite + React + Typescript +++</h1>
+        <div className="card">
+          <button
+            onClick={async () => {
+              setCount((c) => c + 1);
+            }}
+          >
+            count is {count}
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }

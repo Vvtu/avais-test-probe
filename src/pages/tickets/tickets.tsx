4de618e6d52e6645763bd8e41da21e11bcd9f7b9
vvtuev <vvtuev@gmail.com>
2023-10-11 10:05:11 +0300

    fix sorting and filtering

--- src/pages/tickets/tickets.tsx
+++ src/pages/tickets/tickets.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useMemo } from 'react';
 
-import classNames from 'classnames';
+import { useSearchParams } from 'react-router-dom';
 
+import { SORTING_PARAM, TRANSFER_PARAM, ISortingParam } from '@/app-constants';
 import { useGetTickets } from '@/hooks/query-hooks';
 import { ITicket } from '@/services/ticket-validation-scheme';
 
@@ -9,6 +10,8 @@ import { OneTicket } from './one-ticket';
 import cssStyles from './tickets.css?inline';
 
 export function Tickets() {
+  const [searchParams] = useSearchParams();
+
   const result = useGetTickets();
   console.log('[31m ----------- result = ', result); //TODO - delete vvtu
 
@@ -30,15 +33,60 @@ export function Tickets() {
     return arr;
   }, [result.data?.pages]);
 
-  const pagesSortedAndFiltered = useMemo(() => {
-    return [...allPages].sort((a, b) => {
-      if (a.price > b.price) {
-        return 1;
-      }
+  const transferParam000 = parseInt(searchParams.get(TRANSFER_PARAM) ?? '', 10);
+  const transferParam = isNaN(transferParam000) ? -1 : transferParam000;
+  const sortingParam = searchParams.get(SORTING_PARAM) ?? ISortingParam.cheep;
 
-      return -1;
-    });
-  }, [allPages]);
+  const pagesSortedAndFiltered = useMemo(() => {
+    return [...allPages]
+      .filter((item) => {
+        if (transferParam === -1) {
+          return true;
+        } else {
+          const numberOfStops = item.segments[0].stops.length;
+
+          return numberOfStops === transferParam;
+        }
+      })
+      .sort((a, b) => {
+        switch (sortingParam) {
+          case ISortingParam.cheep:
+            if (a.price > b.price) {
+              return 1;
+            } else {
+              if (a.price < b.price) {
+                return -1;
+              } else {
+                if (a.segments[0].duration > b.segments[0].duration) {
+                  return 1;
+                } else {
+                  return -1;
+                }
+              }
+            }
+
+          case ISortingParam.speed:
+            if (a.segments[0].duration > b.segments[0].duration) {
+              return 1;
+            } else {
+              if (a.segments[0].duration < b.segments[0].duration) {
+                return -1;
+              } else {
+                if (a.price > b.price) {
+                  return 1;
+                } else {
+                  return -1;
+                }
+              }
+            }
+          case ISortingParam.optimal: {
+            return 1;
+          }
+          default:
+            return 1;
+        }
+      });
+  }, [allPages, sortingParam, transferParam]);
 
   // const isLoading = result.isFetching || result.isFetchingNextPage;
 
